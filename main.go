df1d5e428348c5aef9cd69f1e9c1c4d50938e74d
JFMajer <jakubmajer@live.com>
2023-01-07 14:37:44 +0100

    added template directory and html render function

--- main.go
+++ main.go
@@ -2,20 +2,35 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
 
+const portNumber = ":8080"
+
 // home page handler
 func homePage(w http.ResponseWriter, r *http.Request) {
-	html := `<strong>Hello, world</strong>`
-	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, html)
+	renderTemplate(w, "home.page.tmpl")
+}
+
+func playRound(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedTemplate.Execute(w, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
 }
 
 func main() {
+	http.HandleFunc("/play", playRound)
 	http.HandleFunc("/", homePage)
 
-	log.Println("starting web server...")
-	http.ListenAndServe(":8080", nil)
+	fmt.Printf("Starting application on port %s\n", portNumber)
+	http.ListenAndServe(portNumber, nil)
 }
